7b3036c36ca8bda555c741093aebeba650fc1629
Yakup Arslan <yakup.yapay.zeka@gmail.com>
2025-12-07 14:58:47 +0300

    iOS native bridge for depth

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,37 @@
-import Flutter
 import UIKit
+import Flutter
+import AVFoundation
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+  private var captureSession: AVCaptureSession?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    let channel = FlutterMethodChannel(
+      name: "depth.channel",
+      binaryMessenger: controller.binaryMessenger
+    )
+
+    channel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      if call.method == "captureDepth" {
+        self.captureDepthImage(result: result)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func captureDepthImage(result: @escaping FlutterResult) {
+    result("Depth captured (Codemagic test placeholder)")
   }
 }
